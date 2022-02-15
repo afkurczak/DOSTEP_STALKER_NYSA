c2a536ddad5f02d88acbc67899288db98cffa2e6
afkurczak <99491505+afkurczak@users.noreply.github.com>
2022-02-15 05:16:27 +0100

    Update kontrola_dostepu.ino

--- kontrola_dostepu/kontrola_dostepu.ino
+++ kontrola_dostepu/kontrola_dostepu.ino
@@ -17,14 +17,40 @@ MFRC522::StatusCode status;
 
 const byte ROWS = 4;
 const byte COLS = 4;
+int x = 4;
 int blockNum = nr_block;
 byte blockData [len_block];
 byte readBlockData[len_block];
 byte bufferLen = len_block;
 byte rowPINS[ROWS] = {13, 12, 11, 10};
 byte colPINS[COLS] = {9, 8, 7, 6};
-char key_buf = "Z"; 
-char read_password;
+char key_buff = 'D'; 
+int master_card1;
+int master_card2;
+int master_card3;
+int master_card4;
+
+byte block[8]={
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+  };
+
+byte frame[8]={
+    B11111,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B10001,
+    B11111,
+  };
 
 char keys [ROWS][COLS] = {
   {'1','2','3','A'},
@@ -40,8 +66,11 @@ void setup() {
 lcd.init ();
 lcd.backlight();
 lcd.setCursor (0, 0);
+lcd.clear();
 SPI.begin();
 mfrc522.PCD_Init();
+lcd.createChar(0, block);
+lcd.createChar(1, frame);
 }
 
 void ReadDataFromBlock(int blockNum, byte readBlockData[])
@@ -66,12 +95,14 @@ void RFID (){
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
+ 
   ReadDataFromBlock(blockNum, readBlockData);
-  for (byte j = 0; j < 16; j++){
-  lcd.setCursor(j,0);
-  read_password = readBlockData[j];
-  lcd.print (read_password);
-  }
+
+  master_card1 = readBlockData[1];
+  master_card2 = readBlockData[2];
+  master_card3 = readBlockData[3];
+  master_card4 = readBlockData[4];
+
   delay(100);
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
@@ -83,28 +114,50 @@ void menu ()
   lcd.setCursor (0,0);
   lcd.print("A: KOD     |");
   lcd.setCursor (0,1);
-  lcd.print("B: KLUCZ   |");
+  lcd.print("B: KARTA   |");
   lcd.setCursor (0,2);
-  lcd.print("C: KARTA   |");
+  lcd.print("C: KLUCZ   |");
   lcd.setCursor (0,3);
-  lcd.print("D: RESET   |");
-  
+  lcd.print("D: MENU    |");
 }
 
 void kod ()
 {
  lcd.setCursor (0,0);
  lcd.print ("PODAJ KOD");
-// if (key_push == 'D'){key_buf = "Z";}
 }
- 
-void loop() {
-  char key_push = keypad.getKey();
-  key_buf = key_push;
-// if (key_push == 'Z'){menu ();}
-// if (key_push == 'A'){kod ();}
+
+void karta ()
+{
+  RFID();
   lcd.setCursor (0,0);
-  lcd.print(key_push);
+  lcd.print ("ZBLIZ KARTE DOSTEPU");
+  lcd.setCursor (0, 1);
+  lcd.print ("DOSTEPNE:     1234");
+  lcd.setCursor(14,2); 
+  if (master_card1 == 'X') {lcd.write(0);} else {lcd.write(1);}
+  lcd.setCursor(15,2);
+  if (master_card2 == 'X') {lcd.write(0);} else {lcd.write(1);}
+  lcd.setCursor(16,2);
+  if (master_card3 == 'X') {lcd.write(0);} else {lcd.write(1);}
+  lcd.setCursor(17,2);
+  if (master_card4 == 'X') {lcd.write(0);} else {lcd.write(1);}
+}
+
+void klucz ()
+{
+ lcd.setCursor (0,0);
+ lcd.print ("KLUCZ    "); 
+}
+
+void loop() {
+ char key_push = keypad.getKey();
+ if (key_push){key_buff = key_push; lcd.clear();}
+ if (key_buff == 'A'){kod();}
+ if (key_buff == 'B'){karta();}
+ if (key_buff == 'C'){klucz();}
+ if (key_buff == 'D'){menu();}
+
 
 //RFID ();
 }
